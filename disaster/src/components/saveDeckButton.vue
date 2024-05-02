bc88a9e5dd4af9735399f84746a7a9f595362d08
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-05-02 16:52:03 -0400

    button made?

---
+++ disaster/src/components/saveDeckButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <button class="button" @click="saveDeck">Save</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let deck = ref('')
+
+async function saveDeck() {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  }
+
+  try {
+    const response = await fetch('http://localhost:8000/add', requestOptions)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json()
+    console.log(data)
+    console.log('success!!')
+  } catch (error) {
+    console.error('problem', error)
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>

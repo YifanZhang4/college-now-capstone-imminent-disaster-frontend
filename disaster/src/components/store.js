0e99781887660d8a9fb7748de246af346ac4dd7e
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2024-05-16 16:37:32 -0400

    pinia

--- disaster/src/components/store.js
+++ disaster/src/components/store.js
@@ -1,9 +1,1 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.mount('#app')
+import { defineStore } from 'pinia'

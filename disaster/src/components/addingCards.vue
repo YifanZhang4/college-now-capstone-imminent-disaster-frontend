0a40d5296ecdcef950823f861a3e3c942ebc15af
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-05-16 16:42:57 -0400

    dootdoot

--- disaster/src/components/addingCards.vue
+++ disaster/src/components/addingCards.vue
@@ -11,26 +11,18 @@
         />
       </div>
       <div class="cardsContainer">
-        <div
-          class="cards"
-          v-for="card in cards"
-          :key="card.id"
-          @click="() => interactCard(card.id)"
-        >
-          <img :src="card.images.small" :alt="card.name" srcset="" />
+        <div class="cards" v-for="card in cards" :key="card" @click="() => interactCard(card.id)">
+          <img :src="card.images.small" :alt="card.name" />
         </div>
+        <button v-if="currentPage < 25" @click="nextPage">Next</button>
+        <button v-if="currentPage >= 2" @click="lastPage">Back</button>
       </div>
     </div>
     <div id="deck">
       <h1>In deck</h1>
       <div class="deckContainer">
-        <div
-          class="cards"
-          v-for="card in inDeck"
-          :key="card.id"
-          @click="() => interactCard(card.id)"
-        >
-          <img :src="card.images.small" :alt="card.name" srcset="" />
+        <div class="cards" v-for="card in inDeck" :key="card" @click="() => interactCard(card.id)">
+          <img :src="card.images.small" :alt="card.name" />
         </div>
       </div>
     </div>
@@ -44,6 +36,7 @@ let input = ref('')
 let cards = ref([])
 let allCards = ref([])
 let inDeck = ref([])
+let currentPage = 1
 
 const getCards = async () => {
   const requestOptions = {
@@ -52,15 +45,22 @@ const getCards = async () => {
       'X-Api-Key': 'process.env.APIKEY'
     }
   }
-
   try {
-    const res = await fetch('https://api.pokemontcg.io/v2/cards', requestOptions)
+    const res1 = await fetch(`https://api.pokemontcg.io/v2/cards`, requestOptions)
+    if (!res1.ok) {
+      throw new Error(`HTTP error! status: ${res1.status}`)
+    }
+    const res = await fetch(
+      `https://api.pokemontcg.io/v2/cards/?pageSize=10&page=${currentPage}`,
+      requestOptions
+    )
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`)
     }
+    const data1 = await res1.json()
     const data = await res.json()
     cards.value = data.data
-    allCards.value = data.data
+    allCards.value = data1.data
     console.log('success!!')
     return data
   } catch (error) {
@@ -83,7 +83,7 @@ const filterCards = () => {
 const interactCard = (cardId) => {
   const inAllCards = !!allCards.value.find((card) => card.id.includes(cardId))
   const cardIndex = allCards.value.findIndex((card) => card.id.includes(cardId))
-  console.log(cardIndex)
+  // console.log(cardIndex)
   const cardInAllCards = allCards.value.find((card) => card.id.includes(cardId))
   if (inAllCards === false) {
     allCards.value.push(cardInAllCards)
@@ -96,11 +96,22 @@ const interactCard = (cardId) => {
     // console.log('Card added to deck', cardInAllCards)
     allCards.value.splice(cardIndex, 1)
     // console.log('Card removed from all', cardInAllCards)
+    sessionStorage.setItem('inDeck', JSON.stringify(inDeck.value))
     return
   } else {
     console.log('probl;em found, please dont tell me about it')
     return
   }
+}
+
+const nextPage = async () => {
+  currentPage++
+  getCards()
+}
+
+const lastPage = async () => {
+  currentPage--
+  getCards()
 }
 
 onMounted(() => {
@@ -122,6 +133,7 @@ onMounted(() => {
   padding-left: 0.5rem;
   background-color: gray;
   width: 15rem;
+  padding-bottom: 0.5rem;
 }
 
 .searchBar {
@@ -139,6 +151,24 @@ onMounted(() => {
   right: 0;
   padding-top: 2rem;
   width: 15rem;
+  height: 94vh;
+  overflow-y: auto;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: white;
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: lightgrey;
+  border-radius: 10px;
 }
 
 h1 {
